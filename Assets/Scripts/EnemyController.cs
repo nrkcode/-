d88a5cd194865f85db8683e971506c588c1a0e35
narim <0018823nmn@naver.com>
2024-09-03 20:55:18 +0900

    Feat: "변경 EnemyController"
    state를 1로 설정하는 코드 & bullet에 맞으면 일정 시간 후 파괴되게 함

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    Animator animator;
+    bool onDead;
+    float time;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        onDead = false;
+        time = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (onDead)
+        {
+            time += Time.deltaTime;
+        }
+        if(time > 0.6f)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("bullet"))
         {
-            Destroy(gameObject);
+            animator.SetInteger("State", 1);
+            OnDead();
         }
+    }
+
+    private void OnDead()
+    {
+        onDead = true;
+        // 스코어 증가 코드 작성
     }
 }
